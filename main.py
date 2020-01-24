15a9749a1a7e0890948a50d6aa3c1a870800fd6d
First Last <Sample@sample.com>
2020-01-24 15:06:22 -0500

    minimum code required

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #custom libraries
 
 #SDS libraries
-from Designable.ProcessableComponent import ProcessableComponent as DPC
-import componentedit
+#from Designable.ProcessableComponent import ProcessableComponent as DPC
+#import componentedit
 
 #mem1.Type (mem1.type) returns "Beam" or "Column" or "Vertical Brace" or "Horizontal Brc" or "Joist" or "Girt" or "Purlin" or "Misc" or "Stair" or "Custom" (read-only)
 
@@ -10,14 +10,15 @@ import componentedit
 
 
 #@compVersions
-class Main_Comp(DPC, componentedit.MemberEditMethods):
+class Main_Comp:
     """
     Include a brief description of the component
     """
 
     def __init__(self, *args, **kwargs):
-       DPC.__init__(self, **kwargs)
-       KWArgMixin.__init__(self, **kwargs)
+       #DPC.__init__(self,None)
+       #componentedit.MemberEditMethods.__init__(self, **kwargs)
+       print(None)
 
 
 
