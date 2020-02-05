600d87a9e91b19ef0cd88469c292ae9bef430acb
First Last <Sample@sample.com>
2020-02-05 17:03:28 -0500

    fixed find material

--- designMainMaterial.py
+++ designMainMaterial.py
@@ -3,10 +3,10 @@ import math
 
 from Designable.Proxies import Hole
 from Layout3D import Layout3D
+from mtrl_list import MtrlLocate
 from Transform3D import Transform3D
 from job import Job
 from sds2 import obj
-
 def design_material(self):
 	atts = {
 	'pt1' : self.Point1,#base_pt
@@ -26,8 +26,8 @@ def design_material(self):
 	}
 
 	proxy = Hole(**atts)
-	proxy.SlotLength = proxy.calc_slot_lenght()
-	proxy.Diameter = proxy.CalculateHoleSize()
+	proxy.SlotLength = proxy.calc_slot_length()
+	proxy.Diameter = proxy.calc_hole_size()
 	proxy.BothSides = 'Yes'
 	proxy.Material = MtrlLocate('Locate material')
 
